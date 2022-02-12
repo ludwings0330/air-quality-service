6285dc0776bf604cb20a80c3fa730dbcb9940605
ludwings <ludwings0330@gmail.com>
2022-02-12 14:47:49 +0900

    refactor: 3차 과제 리팩토링
    - AirQualityResFactory 를 통해 AirQualityResDto 생성
    - 시/도를 추가할때 AirQualityService 의 변경없이 AirQualityResFactory 변경

--- src/main/java/com/example/airqualityservice/AirQualityResFactory.java
+++ src/main/java/com/example/airqualityservice/AirQualityResFactory.java
@@ -1,8 +1,33 @@
 package com.example.airqualityservice;
 
+import com.example.airqualityservice.api.busan.BusanAirQualityApiCaller;
+import com.example.airqualityservice.api.seoul.SeoulAirQualityApiCaller;
+import com.example.airqualityservice.controller.dto.AirQualityReqDto;
+import com.example.airqualityservice.controller.dto.AirQualityResDto;
+import com.example.airqualityservice.service.City;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
+@RequiredArgsConstructor
 @Component
 public class AirQualityResFactory {
+    private final SeoulAirQualityApiCaller seoulAirQualityApiCaller;
+    private final BusanAirQualityApiCaller busanAirQualityApiCaller;
 
+    public AirQualityResDto getAirQuality(City city) {
+
+        if (city == City.서울시) {
+            AirQualityResDto airQuality = seoulAirQualityApiCaller.getAirQuality();
+
+            return airQuality;
+        }
+
+        if (city == City.부산시) {
+            AirQualityResDto airQuality = busanAirQualityApiCaller.getAirQuality();
+
+            return airQuality;
+        }
+
+        throw new RuntimeException(city + "시/도 대기질 정보는 준비중입니다.");
+    }
 }
--- src/main/java/com/example/airqualityservice/service/AirQualityService.java
+++ src/main/java/com/example/airqualityservice/service/AirQualityService.java
@@ -1,5 +1,6 @@
 package com.example.airqualityservice.service;
 
+import com.example.airqualityservice.AirQualityResFactory;
 import com.example.airqualityservice.api.busan.BusanAirQualityApiDto;
 import com.example.airqualityservice.controller.dto.AirQualityReqDto;
 import com.example.airqualityservice.controller.dto.AirQualityResDto;
@@ -16,35 +17,17 @@ import static com.example.airqualityservice.utils.AirQualityGradeUtility.*;
 @RequiredArgsConstructor
 @Service
 public class AirQualityService {
-    private final BusanAirQualityApiCaller busanAirQualityApiCaller;
-    private final SeoulAirQualityApiCaller seoulAirQualityApiCaller;
-
+    private final AirQualityResFactory airQualityResFactory;
     public AirQualityResDto getAirQualityInfo(AirQualityReqDto reqDto) {
         City reqCity = reqDto.getCity();
         String reqDistrict = reqDto.getDistrict();
 
-        if (reqCity == City.서울시) {
-            AirQualityResDto airQuality = seoulAirQualityApiCaller.getAirQuality();
+        AirQualityResDto airQuality = airQualityResFactory.getAirQuality(reqCity);
 
-            if (reqDistrict != null) {
-                return airQuality.searchByDistrict(reqDistrict);
-            }
-
-            return airQuality;
+        if (reqDistrict != null) {
+            return airQuality.searchByDistrict(reqDistrict);
         }
 
-        if (reqCity == City.부산시) {
-            AirQualityResDto airQuality = busanAirQualityApiCaller.getAirQuality();
-
-            if (reqDistrict != null) {
-                return airQuality.searchByDistrict(reqDistrict);
-            }
-
-            return airQuality;
-        }
-
-
-
-        throw new RuntimeException(reqDto.getCity() + "대기질 정보는 준비중 입니다.");
+        return airQuality;
     }
 }
